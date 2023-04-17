d70564d42d0b9396b2d4c783dc046d21a0e87cd4
dragonvedb <dragonvedb@gmail.com>
2023-04-17 14:21:51 +0300

    Add almost everything (i forgor to commit)

---
+++ script.js
@@ -0,0 +1,94 @@
+const Player = (name, token, color) => {
+  const getName = () => name;
+  const getToken = () => token;
+  const getColor = () => color;
+
+  return { getName, getToken, getColor };
+};
+
+const playerOne = Player("Pupa", "cross", "blue");
+const playerTwo = Player("Lupa", "circle", "red");
+
+const gameController = (() => {
+  let turnCounter = 1;
+  const getCurrentTurn = () => turnCounter;
+
+  let currentPlayer = playerOne;
+  const getCurrentPlayer = () => currentPlayer;
+  const switchPlayer = () => {
+    currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+    turnCounter += 1;
+  };
+
+  const declareWinner = (winnerNum) => {
+    const winner = winnerNum === 1 ? playerOne : playerTwo;
+    console.log(winner.getName());
+  };
+
+  const victoryCheck = () => {
+    const board = [
+      [2, 1, 2],
+      [1, 2, 1],
+      [2, 0, 0],
+    ];
+    const linesArr = [
+      [board[0][0], board[0][1], board[0][2]],
+      [board[1][0], board[1][1], board[1][2]],
+      [board[2][0], board[2][1], board[2][2]],
+      [board[0][0], board[1][0], board[2][0]],
+      [board[0][1], board[1][1], board[2][1]],
+      [board[0][2], board[1][2], board[2][2]],
+      [board[0][0], board[1][1], board[2][2]],
+      [board[2][0], board[1][1], board[0][2]],
+    ];
+
+    function checkLine(line) {
+      let leader = null;
+      for (let i = 0; i < line.length; i++) {
+        if (line[i] === 0) return null;
+        if (line[i] !== 0 && leader === null) {
+          leader = line[i];
+        } else if (line[i] !== leader) return null;
+      }
+      return leader;
+    }
+
+    for (let i = 0; i < linesArr.length; i++) {
+      const winner = checkLine(linesArr[i]);
+      if (winner) {
+        return declareWinner(winner);
+      }
+    }
+  };
+
+  return { getCurrentTurn, getCurrentPlayer, switchPlayer, victoryCheck };
+})();
+
+const gameBoard = (() => {
+  const board = [];
+
+  for (let i = 0; i < 3; i++) {
+    board.push([]);
+    for (let x = 0; x < 3; x++) {
+      board[i].push(0);
+    }
+  }
+
+  const getBoard = () => board;
+
+  const setToken = (cellRow, cellCol) => {
+    const player = gameController.getCurrentPlayer();
+
+    if (board[cellRow][cellCol] === 0) {
+      board[cellRow][cellCol] = player === playerOne ? 1 : 2;
+      console.log(gameBoard.getBoard());
+      if (gameController.getCurrentTurn >= 5) gameController.victoryCheck;
+      gameController.switchPlayer();
+    } else console.log("INVALID MOVE");
+  };
+
+  return {
+    getBoard,
+    setToken,
+  };
+})();
